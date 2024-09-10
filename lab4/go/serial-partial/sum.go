462ed670805abdce9815fa0bcbecd11f326488ed
Amanda Carolina <84284738+amandacls@users.noreply.github.com>
2024-09-10 15:59:52 -0300

    Update sum.go

--- lab4/go/serial-partial/sum.go
+++ lab4/go/serial-partial/sum.go
@@ -40,6 +40,7 @@ func main() {
 
 	var totalSum int64
 	sums := make(map[int][]string)
+
 	for _, path := range os.Args[1:] {
 		_sum, err := sum(path)
 
@@ -59,4 +60,39 @@ func main() {
 			fmt.Printf("Sum %d: %v\n", sum, files)
 		}
 	}
+
+	//fileFingerprints := make(map[string]int)
+
+	for i := 0; i < len(os.Args); i++ {
+		for j := i + 1; j < len(os.Args); j++ {
+			file1 := os.Args[i]
+			file2 := os.Args[j]
+
+			// Sem funcionar pela ausencia do código do somador de chunks
+
+			// fingerprint1 := sums[file1]
+			// fingerprint2 := sums[file2]
+			// similarityScore := similarity(fingerprint1, fingerprint2)
+			fmt.Printf("Similarity between %s and %s: %.2f%%\n", file1, file2)
+			//, similarityScore*100)
+		}
+	}
+}
+
+func similarity(base, target []int64) float64 {
+	counter := 0
+	targetMap := make(map[int64]bool)
+
+	for _, value := range target {
+		targetMap[value] = true
+	}
+
+	for _, value := range base {
+		if _, exists := targetMap[value]; exists {
+			counter++
+			delete(targetMap, value)
+		}
+	}
+
+	return float64(counter) / float64(len(base))
 }
